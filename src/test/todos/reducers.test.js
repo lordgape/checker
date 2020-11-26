a23747d44fd932ea4e9c8475aa14383bcb9efddb
Chesvic Lordgape <lordgape@gmail.com>
2020-11-26 09:48:24 +0100

    Add unit test for todo reducer

--- src/test/todos/reducers.test.js
+++ src/test/todos/reducers.test.js
@@ -1,13 +1,13 @@
 import {expect} from "chai"
 import {todos} from "../../reducers/todoReducer"
-import { CREATE_TODO } from "../../actions/todoAction"
+import { CREATE_TODO, MARK_TODO_AS_COMPLETED, REMOVE_TODO } from "../../actions/todoAction"
 
 describe("The Todos Reducer", () => {
 
     it("Can add new todos", () => {
         const fakeTodo = {text:"Get paid in Dollars",isCompleted:false}
         const fakeAction = {
-            action : CREATE_TODO,
+            type : CREATE_TODO,
             payload: fakeTodo
         }
 
@@ -18,5 +18,39 @@ describe("The Todos Reducer", () => {
         const actual = todos(originalState,fakeAction);
 
         expect(actual).to.deep.equal(expected);
+    })
+
+    it("Can remove a todo", () => {
+        const fakeTodo = {text:"Earn $16000 monthly", isCompleted: true, id:1}
+
+        const fakeAction = {
+            type: REMOVE_TODO,
+            payload:fakeTodo
+        }
+
+        const originalState = {isLoading:false,data:[fakeTodo]}
+
+        const expected = {isLoading:false,data:[]}
+
+        const actual = todos(originalState,fakeAction)
+
+        expect(actual).to.deep.equal(expected);
+    })
+
+    it("Can update a todo's isCompleted state to true",() => {
+            const fakeTodo = {text:"Be the best in IT", isCompleted: false, id: 1}
+
+            const fakeAction = {
+                type:MARK_TODO_AS_COMPLETED,
+                payload:fakeTodo
+            }
+
+            const originalState = {isLoading:false,data:[fakeTodo]}
+
+            const expected = {isLoading:false,data:[{...fakeTodo,isCompleted:true}]}
+
+            const actual = todos(originalState,fakeAction);
+
+            expect(actual).to.deep.equal(expected);
     })
 })
